fdb0ea8bec4853b91df457bf5dd1e5ca404335ed
nityadyuthi <nityaa55@gmail.com>
2020-11-17 22:52:48 +0530

    Few Changes

---
+++ README.md
@@ -0,0 +1,64 @@
+# Automatic Ticketing System
+
+A full-stack web application to create a user interface for a metro ticketing system.
+
+## Technologies
+
+- Node.js
+- Expressjs
+- MySQL
+- Bootstrap
+- FaceNet
+- OpenCV, mtcnn ,numpy
+
+## Installation
+
+To use/try out this website, you can download the code and run
+
+```bash
+npm install
+```
+
+or
+
+```bash
+npm i
+```
+
+if you have [Node](https://nodejs.org/en/download/) installed on your system, of course.
+And to run the project on localhost,
+
+```bash
+nodemon app.js
+```
+
+since I have used nodemon. Alternatively,
+
+```bash
+node app.js
+```
+
+can also be used to run the code
+
+Also, you will have to create an account on [RemoteMySQL](http://remotemysql.com) or any other SQL Service and **add the profile info in the database connection in app.js**
+
+## Usage
+
+The database is hosted for free at [RemoteMySQL](http://remotemysql.com) and administered using phpMyAdmin.
+
+Note: Basic auth is used. Also, make sure to give a stronger password for the **very secret** username, and make sure to make it an environment var.
+
+## Contributing
+
+For major changes, please open an issue first to discuss what you would like to change.
+
+There is a lot to change in the project, specially authorisation.
+
+Contributions of any kind are welcome 😄✨✨
+
+For further communication, you can contact the authors:
+
+- [Dyuthi](mailto:nityaa55@gmail.com) => website
+- [Likith](mailto:likithsrinath2000@gmail.com) => website
+- [Khushwinder] (mailto:khushwinder99@gmail.com) => facial recognition
+- [Bhumika] (mailto:bhumidshetty2000@gmail.com) => facial recognition
--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 let mysql = require("mysql");
-const multer = require('multer');
+const multer = require("multer");
 const app = express();
-var upload = multer({ dest: 'uploads/' })
+var upload = multer({ dest: "uploads/" });
 
-var cpUpload = upload.fields([{ name: 'avatar', maxCount: 8 }])
+var cpUpload = upload.fields([{ name: "avatar", maxCount: 8 }]);
+var loggedIn = false;
 
 let connection = mysql.createConnection({
   host: "remotemysql.com",
@@ -33,17 +34,35 @@ app.post("/", (req, res) => {
   const name = req.body.userName;
   const pw = req.body.pw;
   if (name === "admin" && pw === "root") {
-    res.render("home", { status: "ok" });
+    res.render("home");
+    loggedIn = true;
   }
 });
 
+app.get("/logout", (req, res) => {
+  isLoggedIn = false;
+  res.redirect("/");
+});
+
 app.get("/home", (req, res) => {
+  if (loggedIn) {
+    res.render("home");
+  } else {
+    res.redirect("/");
+  }
+});
+app.post("/home", (req, res) => {
+  console.log(loggedIn);
   res.render("home");
 });
 
-//_______________________________ADD PASSENGER_______________________________//
+//__________ADD PASSENGER__________//
 app.get("/addPassenger", (req, res) => {
-  res.render("addPassenger");
+  if (loggedIn) {
+    res.render("addPassenger");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.post("/addPassenger", cpUpload, (req, res) => {
@@ -52,25 +71,35 @@ app.post("/addPassenger", cpUpload, (req, res) => {
   const PhoneNo = req.body.number;
   const EmailID = req.body.email;
   let file = "";
-  req.files['avatar'].forEach(ele => {
+  req.files["avatar"].forEach((ele) => {
     var name = ele.filename.toString();
     file = file.concat(name);
-  })
-  console.log(file)
-  connection.query("INSERT INTO PassengerDetails (PFName,PLName,PhoneNo,EmailID,FileID) VALUES (?)", [[PFName, PLName, PhoneNo, EmailID, file]], (error, result) => {
-    if (error) throw error;
-    else {
-      res.render("success", { message: "Added Passenger into Database succesfully" });
-    }
   });
+  console.log(file);
+  connection.query(
+    "INSERT INTO PassengerDetails (PFName,PLName,PhoneNo,EmailID,FileID) VALUES (?)",
+    [[PFName, PLName, PhoneNo, EmailID, file]],
+    (error, result) => {
+      if (error) throw error;
+      else {
+        res.render("success", {
+          message: "Added Passenger into Database succesfully",
+        });
+      }
+    }
+  );
 });
 
-//_______________________________ISSUE TICKET_______________________________//
+//__________ISSUE TICKET__________//
 app.get("/issueTicket", (req, res) => {
-  connection.query("select * from Stations", (error, result, fields) => {
-    if (error) throw error;
-    res.render("./issueTicket", { data: result });
-  });
+  if (loggedIn) {
+    connection.query("select * from Stations", (error, result, fields) => {
+      if (error) throw error;
+      res.render("./issueTicket", { data: result });
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.post("/issueTicket", (req, res) => {
@@ -96,36 +125,44 @@ app.post("/issueTicket", (req, res) => {
   );
 });
 
-//_______________________________DELETE PASSENGER_______________________________//
+//__________DELETE PASSENGER__________//
 app.get("/deletePassenger", (req, res) => {
-  connection.query(
-    "select * from PassengerDetails",
-    (error, result, fields) => {
-      if (error) throw error;
-      res.render("deletePassenger", { data: result });
-    }
-  );
+  if (loggedIn) {
+    connection.query(
+      "select * from PassengerDetails",
+      (error, result, fields) => {
+        if (error) throw error;
+        res.render("deletePassenger", { data: result });
+      }
+    );
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.post("/deletePassenger", (req, res) => {
   const pid = req.body.PID;
-  var message = "";
   connection.query(
     "DELETE FROM PassengerDetails WHERE PID=" + connection.escape(pid),
     (error, result, fields) => {
       if (error) throw error;
       else {
-        message =
-          "Deleted details of " + req.body.PFName + " " + req.body.PLName;
+        var message =
+          "Deleted details of Passenger ID " + connection.escape(pid);
+        console.log(result);
         res.render("./success", { message: message });
       }
     }
   );
 });
 
-//_______________________________UPDATE PASSENGER DETAILS_______________________________//
+//__________UPDATE PASSENGER DETAILS__________//
 app.get("/update", (req, res) => {
-  res.render("update");
+  if (loggedIn) {
+    res.render("update");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.post("/update", (req, res) => {
@@ -134,32 +171,41 @@ app.post("/update", (req, res) => {
   const PLName = req.body.lname;
   const PhoneNo = req.body.number;
   const EmailID = req.body.email;
-  connection.query("UPDATE PassengerDetails SET PFName=" +
-    connection.escape(PFName) +
-    ",PLName=" +
-    connection.escape(PLName) +
-    ",PhoneNo=" +
-    connection.escape(PhoneNo) +
-    ",EmailID=" +
-    connection.escape(EmailID) +
-    "WHERE PID=" +
-    connection.escape(PID), (error, result) => {
+  connection.query(
+    "UPDATE PassengerDetails SET PFName=" +
+      connection.escape(PFName) +
+      ",PLName=" +
+      connection.escape(PLName) +
+      ",PhoneNo=" +
+      connection.escape(PhoneNo) +
+      ",EmailID=" +
+      connection.escape(EmailID) +
+      "WHERE PID=" +
+      connection.escape(PID),
+    (error, result) => {
       if (error) throw error;
       else {
-        res.render("success", { message: "Updated Passenger in Database succesfully" });
+        res.render("success", {
+          message: "Updated Passenger in Database succesfully",
+        });
       }
-    });
-});
-
-//_______________________________VIEW PASSENGER DETAILS_______________________________//
-app.get("/viewPassengerDetails", (req, res) => {
-  connection.query(
-    "select * from PassengerDetails",
-    (error, result, fields) => {
-      if (error) throw error;
-      res.render("./viewPassengerDetails", { data: result });
     }
   );
+});
+
+//__________VIEW PASSENGER DETAILS__________//
+app.get("/viewPassengerDetails", (req, res) => {
+  if (loggedIn) {
+    connection.query(
+      "select * from PassengerDetails",
+      (error, result, fields) => {
+        if (error) throw error;
+        res.render("./viewPassengerDetails", { data: result });
+      }
+    );
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.post("/viewPassengerDetails", (req, res) => {
@@ -168,15 +214,34 @@ app.post("/viewPassengerDetails", (req, res) => {
     "SELECT * FROM PassengerDetails WHERE PID=" + connection.escape(pid),
     (error, result, fields) => {
       if (error) throw error;
-      connection.query("SELECT * FROM TravelRecord WHERE PID=" + connection.escape(pid), (error1, result1, fields1) => {
-        if (error1) throw error1;
-        connection.query("SELECT * FROM Stations", (error2, result2, fields2) => {
-          if (error2) throw error2;
-          console.log(result1)
-          res.render("passengerDetails", { data: result, data1: result1, data2: result2 });
-        });
-      });
-    });
+      connection.query(
+        "SELECT * FROM TravelRecord WHERE PID=" + connection.escape(pid),
+        (error1, result1, fields1) => {
+          if (error1) throw error1;
+          connection.query(
+            "SELECT * FROM Stations",
+            (error2, result2, fields2) => {
+              if (error2) throw error2;
+              console.log(result1);
+              res.render("passengerDetails", {
+                data: result,
+                data1: result1,
+                data2: result2,
+              });
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 app.listen(process.env.PORT || 3000, () => {
